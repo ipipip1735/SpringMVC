a9f0747dd6f71ee7ed8e5469fe8fac487f1195ce
ipipip1735 <ipipip1735@163.com>
2019-09-20 06:18:27 +0800

    Update Validator

--- src/config/ServletConfig.java
+++ src/config/ServletConfig.java
@@ -3,10 +3,13 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.Validator;
+
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.validation.Validator;
+
 
 /**
  * Created by Administrator on 2019/8/31 16:05.
@@ -28,6 +31,8 @@ public class ServletConfig{
 
     @Bean
     public Validator localValidatorFactoryBean() {
+        System.out.println("~~" + getClass().getSimpleName() + ".localValidatorFactoryBean~~");
+
         LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
         return localValidatorFactoryBean;
     }
--- src/dao/Teacher.java
+++ src/dao/Teacher.java
@@ -1,29 +1,35 @@
 package dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.Validator;
-import org.springframework.validation.annotation.Validated;
+
+
+import javax.validation.Validator;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 /**
  * Created by Administrator on 2019/9/18 13:34.
  */
 public class Teacher {
 
-//    @Autowired
-//    private Validator validator;
+    @Autowired
+    private Validator validator;
 
+    @Min(value = 10, message = "xxxx")
     int age;
 
+    @Min(value = 10, message = "xxxx")
     public int getAge() {
         System.out.println("~~" + getClass().getSimpleName() + ".getAge~~");
 
         return age;
     }
 
+    @Min(value = 10, message = "xxxx")
     public void setAge(int age) {
         System.out.println("~~" + getClass().getSimpleName() + ".setAge~~");
 
-//        System.out.println(validator);
+        System.out.println(validator);
         this.age = age;
     }
 }
