a3f79772934d34d1a88278c027f043a70f0e248e
ipipip1735 <ipipip1735@163.com>
2019-09-27 06:20:13 +0800

    Update URI

--- src/controller/UriController.java
+++ src/controller/UriController.java
@@ -17,76 +17,82 @@ import java.net.URI;
 public class UriController {
 
 
-    @GetMapping("/uri")
-    public void uri(Model model) {
-
-
-        //简单使用
-        UriComponents uriComponents = UriComponentsBuilder
-                .fromUriString("https://example.com/hotels/{hotel}")
-                .queryParam("q", "{q}")
-                .build();
-        URI uri = uriComponents.expand("Westin", "123").toUri();
-        System.out.println(uri);
-
-
-
-        //简化使用
-//        URI uri = UriComponentsBuilder
+//    @GetMapping("/uri")
+//    public void uri(Model model) {
+//
+//
+//        //简单使用
+//        UriComponents uriComponents = UriComponentsBuilder
 //                .fromUriString("https://example.com/hotels/{hotel}")
 //                .queryParam("q", "{q}")
-//                .build("Westin", "123");
-
-
-
-        //使用工厂
-//        String baseUrl = "https://example.com";
-//        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(baseUrl);
+//                .build();
+//        URI uri = uriComponents.expand("Westin", "123").toUri();
+//        System.out.println(uri);
 //
-//        URI uri = uriBuilderFactory.uriString("/hotels/{hotel}")
+//
+//        //简化使用
+////        URI uri = UriComponentsBuilder
+////                .fromUriString("https://example.com/hotels/{hotel}")
+////                .queryParam("q", "{q}")
+////                .build("Westin", "123");
+//
+//
+//        //使用工厂
+////        String baseUrl = "https://example.com";
+////        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(baseUrl);
+////        URI uri = uriBuilderFactory.uriString("/hotels/{hotel}")
+////                .queryParam("q", "{q}")
+////                .build("Westin", "123");
+//
+//
+//        //使用 REST URL
+////        String baseUrl = "https://example.org";
+////        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(baseUrl);
+////        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.TEMPLATE_AND_VALUES);
+////
+////        RestTemplate restTemplate = new RestTemplate();
+////        restTemplate.setUriTemplateHandler(factory);
+////        restTemplate.get
+//
+//
+//        model.addAttribute("uri", uri);
+//    }
+
+
+//    @GetMapping("/uri")
+//    public void webClinet(Model model) {
+//
+////        String baseUrl = "https://example.org";
+////        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(baseUrl);
+////        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.TEMPLATE_AND_VALUES);
+////        WebClient
+//    }
+
+
+
+    @GetMapping("/uri")
+    public void uriEncoding(Model model) {
+
+        //URI编码
+//        URI uri = UriComponentsBuilder.fromUriString("https://example.com/ho:t;e&l s/{hotel}")
 //                .queryParam("q", "{q}")
-//                .build("Westin", "123");
+//                .encode()
+//                .build()
+//                .expand("We:s;t&in", "12<3")
+//                .toUri();
 
 
-
-        //使用REST URL
-//        String baseUrl = "https://example.org";
-//        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(baseUrl);
-//        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.TEMPLATE_AND_VALUES);
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.setUriTemplateHandler(factory);
-//        restTemplate.get
-
-
+        URI uri = UriComponentsBuilder.fromUriString("https://example.com/ho:t;e&l s/{hotel}")
+                .queryParam("q", "{q}")
+                .build()
+                .expand("We:s;t&in", "12<3")
+                .encode()
+                .toUri();
 
 
         model.addAttribute("uri", uri);
-    }
-
-
-
-    @GetMapping("/uri")
-    public void webClinet(Model model) {
-
-//        String baseUrl = "https://example.org";
-//        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(baseUrl);
-//        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.TEMPLATE_AND_VALUES);
-//        WebClient
-
-
-
-        //简单使用
-        URI uri = UriComponentsBuilder
-                .fromUriString("https://example.com/hotels/{hotel}")
-                .queryParam("q", "{q}")
-                .encode()
-                .build()
-                .expand("Westin", "123")
-                .toUri();
 
     }
-
 
 
 }
