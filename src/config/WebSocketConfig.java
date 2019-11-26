78a83a16b6e358e674585ebaebe0b154b83e47fc
ipipip1735 <ipipip1735@163.com>
2019-11-26 17:34:15 +0800

    Update WebSocket

--- src/config/WebSocketConfig.java
+++ src/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package config;
 
 import dao.Person;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
@@ -10,9 +11,18 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
@@ -27,27 +37,75 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 /**
  * Created by Administrator on 2019/11/25 17:24.
  */
-public class WebSocketConfig implements WebMvcConfigurer {
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+//    @Bean
+//    public HandlerMapping handlerMapping() {
+//        System.out.println("~~handlerMapping~~");
+//        Map<String, WebSocketHandler> map = new HashMap<>();
+//        map.put("/wss", session -> {
+//            System.out.println("~~websocket.session~~");
+//            System.out.println();
+//            return Mono.never();
+//        });
+//
+//
+//        int order = -1; // before annotated controllers
+//
+//        return new SimpleUrlHandlerMapping(map, order);
+//    }
+
+//    @Bean
+//    public WebSocketHandlerAdapter handlerAdapter() {
+//        System.out.println("~~handlerAdapter~~");
+//
+//        return new WebSocketHandlerAdapter();
+//    }
+
+
+//    @Bean
+//    public WebSocketHandlerAdapter handlerAdapter() {
+//        System.out.println("~~handlerAdapter~~");
+//
+//        return new WebSocketHandlerAdapter();
+//    }
+
+
     @Bean
-    public HandlerMapping handlerMapping() {
-        System.out.println("~~handlerMapping~~");
-        Map<String, WebSocketHandler> map = new HashMap<>();
-        map.put("/wss", session -> {
-            System.out.println("~~websocket.session~~");
-            System.out.println();
-            return Mono.never();
-        });
-
-
-        int order = -1; // before annotated controllers
-
-        return new SimpleUrlHandlerMapping(map, order);
-    }
-
-    @Bean
-    public WebSocketHandlerAdapter handlerAdapter() {
+    public WebSocketHandler myHandler() {
         System.out.println("~~handlerAdapter~~");
 
-        return new WebSocketHandlerAdapter();
+        return new TextWebSocketHandler(){
+            @Override
+            protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+                System.out.println("~~handleBinaryMessage~~");
+                super.handleBinaryMessage(session, message);
+            }
+        };
     }
+
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        System.out.println("~~registerWebSocketHandlers~~");
+
+        registry.addHandler(myHandler(), "/wss");
+    }
+
+
+
+//    @Bean
+//    public DefaultHandshakeHandler handshakeHandler() {
+//
+//        WebSocketPolicy policy = new WebSocketPolicy(WebSocketBehavior.SERVER);
+//        policy.setInputBufferSize(8192);
+//        policy.setIdleTimeout(600000);
+//
+//        return new DefaultHandshakeHandler(
+//                new JettyRequestUpgradeStrategy(new WebSocketServerFactory(policy)));
+//    }
+
+
 }
--- src/controller/WebSocketController.java
+++ src/controller/WebSocketController.java
@@ -18,5 +18,4 @@ public class WebSocketController {
         return "websocket";
     }
 
-
 }
