b9353a159e27abced63ccfdbd126e98338f52ce5
ipipip1735 <ipipip1735@163.com>
2019-12-13 06:41:19 +0800

    Update STOMP

--- src/controller/AsyncController.java
+++ src/controller/AsyncController.java
@@ -22,89 +22,92 @@ import java.util.function.Consumer;
 @RestController
 public class AsyncController {
 
+    @GetMapping("/async")
+    public ListenableFuture<String> async() {
+        System.out.println("async start");
+        System.out.println(Thread.currentThread());
+
+
+        AsyncResult<String> asyncResult = new AsyncResult<>("oo");
+        asyncResult.addCallback(new ListenableFutureCallback<String>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("~~onFailure~~");
+                System.out.println("ex is " + ex);
+                System.out.println(Thread.currentThread());
+            }
+
+            @Override
+            public void onSuccess(String result) {
+                System.out.println("~~onSuccess~~");
+                System.out.println(Thread.currentThread());
+                System.out.println("result is " + result);
+            }
+        });
+
+
+        System.out.println("async end");
+
+        return asyncResult;
+    }
+
+
 //    @GetMapping("/async")
-//    public ListenableFuture<String> flux() {
+//    public DeferredResult<String> async() {
+//        System.out.println("~~async|start~~");
 //
 //
-//        AsyncResult<String> asyncResult = new AsyncResult<>("oo");
-//        asyncResult.addCallback(new ListenableFutureCallback<String>() {
+//        DeferredResult<String> deferredResult = new DeferredResult();
+//
+//        new Thread(new Runnable() {
 //            @Override
-//            public void onFailure(Throwable ex) {
-//                System.out.println("~~onFailure~~");
-//                System.out.println("ex is " + ex);
+//            public void run() {
+//                try {
+//                    Thread.sleep(3000L);
+//
+//
+////                    deferredResult.setResult("ok");
+////                    System.out.println("complete!");
+//
+//
+////                    deferredResult.setErrorResult(new Exception("xxx"));
+////                    System.out.println("complete!");
+//
+//
+//                    throw new Exception("xxx");
+//
+//
+//
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
 //            }
+//        }).start();
 //
+//        deferredResult.onCompletion(()->{
+//            System.out.println("~~onCompletion~~");
+//        });
+//
+//        deferredResult.onTimeout(()->{
+//            System.out.println("~~onTimeout~~");
+//        });
+//
+//        deferredResult.onError(new Consumer<Throwable>() {
 //            @Override
-//            public void onSuccess(String result) {
-//                System.out.println("~~onSuccess~~");
-//                System.out.println("result is " + result);
+//            public void accept(Throwable throwable) {
+//                System.out.println("~~onError~~");
+//                System.out.println(throwable);
 //            }
 //        });
 //
 //
-//        return asyncResult;
+//        System.out.println("~~async|end~~");
+//
+//
+//        return deferredResult;
 //    }
-
-
-
-    @GetMapping("/async")
-    public DeferredResult<String> async() {
-        System.out.println("~~async|start~~");
-
-
-        DeferredResult<String> deferredResult = new DeferredResult();
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(3000L);
-
-
-//                    deferredResult.setResult("ok");
-//                    System.out.println("complete!");
-
-
-//                    deferredResult.setErrorResult(new Exception("xxx"));
-//                    System.out.println("complete!");
-
-
-                    throw new Exception("xxx");
-
-
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
-        deferredResult.onCompletion(()->{
-            System.out.println("~~onCompletion~~");
-        });
-
-        deferredResult.onTimeout(()->{
-            System.out.println("~~onTimeout~~");
-        });
-
-        deferredResult.onError(new Consumer<Throwable>() {
-            @Override
-            public void accept(Throwable throwable) {
-                System.out.println("~~onError~~");
-                System.out.println(throwable);
-            }
-        });
-
-
-        System.out.println("~~async|end~~");
-
-
-        return deferredResult;
-    }
-
-
 
 
 }
